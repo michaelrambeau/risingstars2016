8f285f806ed2750c1e520553cb208af5cf3f98ad
michaelrambeau <mikeair@gmail.com>
2017-01-15 16:44:59 +0900

    Add more comments

--- src/bestof2016/comments/conclusion.js
+++ src/bestof2016/comments/conclusion.js
@@ -18,17 +18,18 @@ So if you want a more qualitative approach, check the results from Sacha Greifâ€
 ---
 
 It's time to think about 2017, where are the next rising stars?
-Here my 10 picks of the year, the projects and the ideas I liked in 2016 and that will keep up growing in 2017:
+
+Here are my 10 picks of the year, the projects and the ideas I liked in 2016 and that will keep up growing in 2017:
 
 * {vuejs}: the momentum will not stop
 * {electron}
 * {create-react-app} (used to build this page!)
 * {react-native}
 * {gatsby}
-* {yarn}: it's nice to see the community working hard to make npm (our every day tool) better
+* {yarn}: a fast, reliable and secure dependency management that can replace npm, read about the state of node.js package managers [here](https://blog.risingstack.com/yarn-vs-npm-node-js-package-managers/)
 * The concept of "Progressive Web Applications"
-* Micro-services with node.js, made easy to deploy with solutions like [Now](https://zeit.co/now), [Webtask.io](https://webtask.io/) or [Stdlib](https://stdlib.com/)
-* The amazing work by the [Zeit](https://zeit.co/) team, especially their hosting solution called [now](https://zeit.co/now) and projects like {nextjs}.
+* Node.js micro-services made easy to deploy with a hosting solution like [Now](https://zeit.co/now)
+* The evolution of {nodejs}: the latest releases support very well the ES6 syntax
 * {graphql}: all my friends told me that something big is coming!
 
 ---
--- src/bestof2016/comments/index.js
+++ src/bestof2016/comments/index.js
@@ -50,17 +50,21 @@ To be exact, we mix in this category 2 kinds of projects:
 
 We have already mentioned {vuejs} (number 1 overall).
 
+#### React and its contenders
+
 {react} is number 2, no front-end developer can ignore React and its rich eco-system.
-
-Angular project has been split into 2 repositories because Angular 2 is a full rewrite of Angular 1, even if some concepts remain the same.
-
-* {angular-2} is written in TypeScript and takes advantage of ES6 to deliver a modern and thorough framework
-* {angularjs} project is the branch 1.x, it's still used in many projects and will remain popular for a while.
 
 {react} is so popular that it inspired a lot of other libraries that aim to take the best of React, without the fat, in order to improve both performance in the browser and building time.
 
 * {inferno} was the most popular project in this category, it claims to be the fatest alternative to React
 * {preact} is a nice alternative too and seems to be quite mature, with an eco-system that includes a boilerplate with offline capabilities, a router, a "compat" module to include easily existing React modules...
+
+#### Angular 1 and 2
+
+Angular project has been split into 2 repositories because Angular 2 is a full rewrite of Angular 1, even if some concepts remain the same.
+
+* {angular-2} is written in TypeScript and takes advantage of ES6 to deliver a modern and thorough framework
+* {angularjs} project is the branch 1.x, it's still used in many projects and will remain popular for a while.
 
 It's worth to mention that {ember}, despite its large eco-system and its community is not in the top 10.
 
@@ -68,30 +72,59 @@ Rather than opting for full frameworks with all features "out of the box", it se
 `
 
 const nodejsframework = `
-When you have to build a web application with node.js, {express} is often considered as the de-facto web server.
+In 2016 it has never been easier to create and deploy a node.js application with solutions like
+
+* [Now](https://zeit.co/now)
+* [Webtask.io](https://webtask.io/)
+* [Stdlib](https://stdlib.com/)
+
+So if you have to build a web application, which framework to choose?
+
+#### {express}
+
+When you build a web application with node.js, {express} is often considered as the de-facto web server.
 Its philosophy (a minimalistic core that you can extend using middleware packages) is familiar to most of node.js developers.
 
-{koa} philosophy is close to {express} but it's built using ES6 generators to avoid a problem sometimes called "The callback hell".
+#### {koa}
 
-{feathers} is a solution to create a "service oriented" architecture, it's a good fit to create node.js microservices
- (by the way micro-service was an other buzz word in 2016)
+{koa} philosophy is close to {express} but it's built using ES6 generators to avoid a problem sometimes called *[ Callback Hell](https://www.sitepoint.com/saved-from-callback-hell/)*.
+
+#### {feathers}
+
+{feathers} is a very flexible solution to create a "service oriented" architecture, it's a good fit to create node.js microservices.
+
+#### {keystone}
 
 {keystone} is one of the best solutions I know to get an admin client up and running, in order to manage the content coming from a MongoDB database.
 The Admin UI is automatically generated from the models, has all CRUD actions and nice filters.
+
+#### {sails}
+
+{sails} is a full MVC framework, very inspired by Ruby on Rails (hence the name **S**ails!). It has been around for a long time. It can play with any kind of database, SQL or no-SQL.
+
+#### {loopback}
+
+{loopback} is an other mature framework with a lot of features built-in, including authentication with token and connector to any kind of database.
+It has a very nice **API explorer** feature that let developers check all API end-points in an intuitive way, with the ability to check any user's token.
+It's definitevely a good choice if you have to build an API.
 `
 
 const mobile = `
 JavaScript is so ubiquitous that you can build native mobile applications using technologies web developers already know (HTML. JavaScript, CSS).
 
-We have seen that {react} is number 2, no front-end developer can ignore React and its rich eco-system. But what is more impressive is the rise of {react-native}.
+#### {react-native}
 
 With React Native, from the same code base, you can build iOS and Android real **native** mobile applications, using concepts familiar to React developers. To know more about building applications for both iOS and Android, read [this tutorial](http://makeitopen.com/).
 
 Other solutions, based on Cordova, used to rely on Webview to render the screens and were not as efficient than a native solution.
-"Write Once Run Everywhere"... a developer's dream came true!
+"Write Once Run Everywhere"... this is a developer's dream came true!
+
+#### {ionic}
 
 {ionic} was a pionner with the concept of "hybrid" applications.
 Under the hood, it's based on Cordova to access the mobile device features. It's very mature with a large eco-system.
+
+#### {nativescript}
 
 {nativescript} aims for the same goal as {react-native} (build real mobile applications using web technologies). It comes in 2 flavors, NativeScript Core and NativeScript + Angular 2
 
@@ -99,21 +132,31 @@ A project to follow closely in 2017: [Weex](https://weex-project.io/), "a framew
 `
 
 const testframework = `
-The 2 more most popular testing framework are {jasmine} and {mocha} but 2 more recent projects got more traction in 2016: {ava} and {jest}.
+The 2 most famous testing framework are {jasmine} and {mocha} but 2 more recent projects got more traction in 2016: {ava} and {jest}.
 
-{ava}, created by the prolific [Sindre Sorhus](https://github.com/sindresorhus) puts emphasize on performance (parallel test) and ES6.
+#### {ava}
 
-{jest} got a lot of traction over the last weeks and is more and more used in the React community.
+{ava}, created by the prolific [Sindre Sorhus](https://github.com/sindresorhus) puts emphasize on performance (parallel test) and ES6. AVA's syntax is close to standard test frameworks like {tape} and {node-tap}.
+
+#### {jest}
+
+{jest}, an other Facebook project, got a lot of traction over the last weeks. It's well-known in the React community, more and more people are moving to Jest (read [this story](https://medium.com/@kentcdodds/migrating-to-jest-881f75366e7e#.z9x53j1ea) for example) and it may become the most popular testing framework in 2017.
+
+Jest has good mocking abilities built-in, whereas other testing frameworks usually rely on libraries like {sinonjs}.
 `
 
 const ide = `
 About IDE ("Integrated development environment"), it's worth to mention that 2 of the most popular IDEs are open-source projects made with web technologies.
+
+#### {visual-studio-code}
 
 In our results, Microsoft leads the way with {visual-studio-code}.
 
 It provides a nice integration with {typescript} and node.js.
 Some developers mention improvement about development speed and thank to the "IntelliSense" feature (a mix of highlighting and autocomplete).
 Saying "open source" and "Microsoft" in the same sentence is no more an oxymoron!
+
+#### {atom}
 
 {atom}, pushed by Github and built with {electron} (like several other desktop applications, including the Slack desktop client) is not far behind {visual-studio-code}. A fun fact about Atom: its main language is CoffeeScript!
 `
@@ -124,30 +167,51 @@ They transform the code into "standard JavaScript" code that the browser (or nod
 
 For example compilers let developers write code using the latest version of JavaScript (ES6) without having to worry about browser support.
 
+#### {typescript}
+
 The most trendy transpiler was {typescript}, it brings to web developers the static types used by Java and C# developers.
 The fact that {angular-2} uses TypeScript added even more traction.
 There are pros and cons about using types in JavaScript, read these 2 article to make your own point of view:
 * [You Might Not Need TypeScript](https://medium.com/javascript-scene/you-might-not-need-typescript-or-static-types-aa7cb670a77b#.1pn05vlis)
 * [TypeScript: the missing introduction](https://toddmotto.com/typescript-the-missing-introduction)
 
+#### {babel}
+
 {babel}, along with Webpack, almost became a standard to compile ES6 code and templates used by libraries like React (JSX) in standard JavaScript. Initially created to compile ES6, it became a much more generic tool that can accomplish any code transformation, thank to a system of plugins.
+
+#### {flow}
 
 {flow} is not a compiler, it's a static type checker used to "annotate" the JavaScript code.
 Basically using Flow inside a code base means adding comments to describe expected types (read more about using Flow to write modules [here](http://javascriptplayground.com/blog/2017/01/npm-flowjs-javascript/)).
 
 It's used inside the code source of Facebook projects. Since Facebook became one of the major actors of the open source world (with projects like {react}, {react-native}, {flux}, {immutable}, {jest}...), that means a lot.
 
+#### {coffeescript}
+
 During years {coffeescript} and its lean syntax, inspired by Python and Ruby syntax, was the most popular compiler but it was less more trendy in 2016, a lot of developers moved from CoffeeScript to ES6 with Babel.
 `
 
 const build = `
 In 2016, it's difficult to imagine a web application without any kind of building process.
+You usually need a building process to compile templates and optimize assets in order to ship your web application in production.
+
+#### {webpack}
 
 {webpack} is the main tool used to build a single-page application, it plays well with the React eco-system. The newly released version 2 comes with some promising enhancements (check this [introduction](https://blog.madewithenvy.com/getting-started-with-webpack-2-ed2b86c68783#.7wyiawc0o))
 
-{gulp} is a generic a task runner that can be used for any kind of automatic process, so it's not a direct contender of Webpack or Browserify, it can play well with webpack even if developers tend to use npm scripts instead.
+#### {gulp}
 
-{browserify}, because of its simplicity is usually loved by node.js developers. Basically it takes several node.js packages as an input and generates one single "build" file for the browser as an output. But it seems that a more opionated tool like {webpack} is a a better fit to web application workflows.
+{gulp} is a generic a task runner that can be used for any kind of automatic process involving the file system, so it's not a direct contender of Webpack or Browserify.
+
+Like {grunt}, Gulp works by aggregation: you can ask it to minify and concatenate a list of assets but it does not deal with modular JavaScript by itself, as {webpack} or {browserify} do.
+
+Nevertheless it can play well with webpack even if developers tend to use npm scripts instead.
+
+#### {browserify}
+
+{browserify}, because of its simplicity is usually loved by node.js developers.
+
+Basically it takes several node.js packages as an input and generates one single "build" file for the browser as an output. But it seems that a more opionated tool like {webpack} is a a better fit to web application workflows.
 
 2 contenders to follow in 2017, that emphasize on performance and simplicity:
   * {rollup}
@@ -160,10 +224,21 @@ So how to start creating a real-world application ?
 
 It's the answer provided by the React "boilerplates" and other "starter kits"...
 
+#### {react-boilerplate}
+
 The well named {react-boilerplate} has everything you need, including {redux} and some nice offline features, taking advantage of the web workers technology.
+
 It let developers create what is called "Progressive Web Applications" (or "PWA"): web application that run offline, using a technology called Service Worker, read [this article](https://ponyfoo.com/articles/progressive-app-serviceworker) from Nicolás Bevacqua)
 
-Facebook addressed the need by providing a lighter approach called {create-react-app} that is a very convenient to start a new React project. Dan Abramov (the creator of Redux, working now for Facebook) did a great job, finding the right balance between simplicity and features.
+#### {create-react-app}
+
+Facebook addressed the need by providing a lighter approach called {create-react-app} that is a very convenient to start a new React project.
+
+[Dan Abramov](https://github.com/gaearon) (the creator of Redux, working now for Facebook) did a great job, finding the right balance between simplicity and features. No fancy styling solution (just plain CSS), no server-side rendering but everything is well packaged.
+
+The main difference with its contenders is that if you use create-react-app, it becomes a dependency of your project, all the magic is hidden and what you see is only **your** application code. You can upgrade the dependency at any time.
+
+#### {nextjs}
 
 {nextjs}, created by the busy folks from [Zeit](https://zeit.co/), has a server-side rendering feaure that can be used to create *universal* applications (or *isomorphic* applications, as we used to say in 2015), that is to say applications that run more or less with the same code client AND server-side.
 
@@ -192,10 +267,12 @@ SSG are very popular because there are a lot of very good solutions to host stat
 * [Surge](https://surge.sh/)
 * [Now static](https://zeit.co/blog/now-static)
 
-About the 2 most trendy (built with JavaScript) SSG in 2016:
+#### {hexo}
 
-{hexo} is a thorough SSG, close to CMS systems like Wordpress, that can be used to build a blog for example.
+In 2016, the most popular SSG built with node.js was {hexo}. It's a thorough SSG, close to CMS systems like Wordpress, that can be used to build a blog for example.
 It has a lot of features including an internationalization plugin.
+
+#### {gatsby}
 
 The newcomer {gatsby} is a very interesting solution, it stands out from its condenders because it uses React eco system to generate static html files. The fact that you can combine React components, mardown files and server-side rendering makes it very powerful.
 `
