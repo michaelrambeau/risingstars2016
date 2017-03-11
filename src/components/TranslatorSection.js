5af59f7f57fab7a45af80ca36fa3ae614b3447e0
michaelrambeau <mikeair@gmail.com>
2017-03-12 07:59:04 +0900

    Fix Chinese page link, add 'Chinese (simplified) translation' section

--- src/components/TranslatorSection.js
+++ src/components/TranslatorSection.js
@@ -4,30 +4,36 @@ import team from '../utils/teamMembers'
 const Section = ({ language }) => (
   <section className="TranslatorSection">
     <div className="container small-container">
-      {language === 'ja' ? <TeamMemberList /> : (
-        <div style={{ textAlign: 'center' }}>
-          <p>
-            This article is also available in <a href="/ja/">Japanese</a>, <a href="/cn/">中文</a>
-          </p>
-          <p>
-            <a href="/ja/">日本語はこちら</a>です,
-            <a href="/cn/"> 简体中文版</a>
-          </p>
-        </div>
-      )}
+      {language === 'en' ? <OtherLanguages /> : <TeamMemberList language={language} />}
     </div>
   </section>
 )
 
-const TeamMemberList = () => {
-  const translators = team.filter(member => member.role === 'translation')
+const OtherLanguages = () => (
+  <div style={{ textAlign: 'center' }}>
+    <p>
+      This article is also available in <a href="/ja/">Japanese</a> and in <a href="/zh/">Chinese (simplified)</a>.
+    </p>
+    <p>
+      <a href="/ja/">日本語はこちら</a>です
+    </p>
+    <p>
+      <a href="/zh/"> 简体中文版</a>
+    </p>
+  </div>
+)
+
+const TeamMemberList = ({ language }) => {
+  const translators = team.filter(
+    member => member.role === 'translation' && member.language === language
+  )
   const author = team.find(member => member.role === 'author')
   return (
     <div>
-      <div style={{ textAlign: 'center', paddingBottom: '2rem' }}>
-        <img src="/img/ja/rising-stars.png" className="img-responsive" alt="Rising Stars 2016" />
-      </div>
-      <TranslatorBlock translators={translators} />
+      {language === 'ja' && <div style={{ textAlign: 'center', paddingBottom: '2rem' }}>
+        <img src={`/img/${language}/rising-stars.png`} className="img-responsive" alt="Rising Stars 2016" />
+      </div>}
+      <TranslatorBlock translators={translators} language={language} />
       <AuthorBlock author={author} />
     </div>
   )
@@ -44,10 +50,15 @@ const AuthorBlock = ({ author }) => (
   </div>
 )
 
-const TranslatorBlock = ({ translators }) => (
+const translations = {
+  'ja': 'Japanese translation',
+  'zh': 'Chinese (simplified) translation'
+}
+
+const TranslatorBlock = ({ translators, language }) => (
   <div>
     <p className="member-list-header">
-    Japanese translation
+      {translations[language]}
     </p>
     <div className="translator-list">
       {translators.map(translator => (
--- src/utils/teamMembers.js
+++ src/utils/teamMembers.js
@@ -7,21 +7,32 @@ const team = [
     avatar: 'https://avatars0.githubusercontent.com/u/442462?v=3&s=75',
     bio1: 'A generic programmer and linguist living in Osaka from Sapporo.',
     bio2: '素朴なプログラマで言語マニア。札幌生まれの大阪在住。',
-    role: 'translation'
+    role: 'translation',
+    language: 'ja'
   },
   {
     name: 'Yohei Ice',
     url: 'https://github.com/meltedice',
     avatar: 'https://avatars3.githubusercontent.com/u/557001?v=3&s=75',
     bio1: 'Web application developer (Rails, React, ...)',
-    role: 'translation'
+    role: 'translation',
+    language: 'ja'
   },
   {
     name: 'Shigeto Yatani',
     url: 'https://www.facebook.com/vanxx2',
     avatar: 'https://avatars2.githubusercontent.com/u/572205?v=3&s=75',
     bio1: 'Node.js, 美味しいご飯, 酒, 料理, バスケ, ヨガ, エレクトロニカ, DJ, 園芸, DIY, プチアウトドア, 六甲山',
-    role: 'translation'
+    role: 'translation',
+    language: 'ja'
+  },
+  {
+    name: 'wangshijun',
+    url: 'http://www.feweekly.com/',
+    avatar: 'https://avatars1.githubusercontent.com/u/719985?v=3&s=75',
+    bio1: 'wangshijun bio',
+    role: 'translation',
+    language: 'zh'
   },
   {
     name: 'Michael Rambeau',
